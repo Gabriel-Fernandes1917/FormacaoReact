a73a1cb2cf90abb8a5f5f5bd211fa2a3859057bd
Gabriel Fernandes <gabgui2001@gmail.com>
2022-07-25 00:23:21 -0300

    .

--- reactTypeScript/aula3 cardapio/aluroni/src/index.tsx
+++ reactTypeScript/aula3 cardapio/aluroni/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "normalize.css" // reset css
 import './index.css';
 import Cardapio from './pages/Cardapio';
 
--- reactTypeScript/aula3 cardapio/aluroni/src/pages/Cardapio/index.tsx
+++ reactTypeScript/aula3 cardapio/aluroni/src/pages/Cardapio/index.tsx
@@ -1,13 +1,32 @@
 import styles from './Cardapio.module.scss';
-import {ReactComponent as Logo} from '../../assets/Logo.svg'
+import {ReactComponent as Logo} from 'assets/Logo.svg' // transform img in one React Component
+import Buscador from './Buscador';
+import { useState } from 'react';
+// for use the img in React you need import she be 
+
 
 export default function Cardapio() {
-    
+    const [busca, setBusca] = useState("")
     return(
         <main>
             <nav className={styles.menu}>
-                <Logo />
+                <Logo /> 
+
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do codigo e da massa
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>
+                    Cardapio
+                </h3>
+                <Buscador 
+                busca={busca}
+                setBusca={setBusca}
+                />
+            </section>
         </main>
     )
 }
